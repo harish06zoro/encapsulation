6e34f8c61dab528a68105bd8d178deea6f51cd08
harish06zoro <harish12ser@gmail.com>
2023-12-05 16:06:01 +0530

    new

---
+++ encapsulation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            vehicle v = new vehicle();
+            v.Regno = "57565";
+            v.Wheels = 4;
+            v.Litre = 10;
+
+
+            Console.WriteLine(v);
+        }
+    }
+}
---
+++ encapsulation/vehicle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace encapsulation
+{
+    class vehicle
+    {
+        private string _regno;
+        int _wheels;
+        int _litre;
+
+
+
+
+
+
+        public int Wheels
+        {
+            get { return _wheels; }
+            set
+            {
+                if (value >= 2 && value <= 20)
+                {
+                    _wheels = value;
+                }
+            }
+        }
+
+        
+
+
+public int Litre
+{
+  get { return _litre; }
+
+  set {
+      
+         if (value>=1&&value<100)
+         {
+      
+      _litre = value;
+  }
+  }
+}
+
+        private string regno
+
+
+
+public string Regno
+{
+  get { return regno; }
+  set { regno = value; }
+}}}
+
